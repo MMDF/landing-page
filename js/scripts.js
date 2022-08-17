4636735b5b7c5b10deebd5e7654574ea68868244
surajk027 <surajkushwaha027@gmail.com>
2022-08-17 23:06:43 +0530

    footer design is changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - New Age v6.0.6 (https://startbootstrap.com/theme/new-age)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -32,3 +23,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+var qna= document.getElementsByClassName("qna");
+for (i in qna){
+    i.addEventListener("click", function() {
+        this.p.style.display = visible;
+        this.br.style.display = visible;
+    }
+    );
+}
